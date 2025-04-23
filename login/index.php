05d2f94a5c982c49dde7e8faa1191e19eae1e115
sabila-a <salsabila.rosi33@smk.belajar.id>
2025-04-23 14:38:41 +0700

    upload pertama

---
+++ login/index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+include 'database.php';
+session_start();
+
+    if (isset($_SESSION['username'])) {
+        header("Location: berhasil_login.php");
+        exit();
+    }
+
+*/
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desa Talagasari</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="login">
+           <form method="post" action="cek_login.php">
+                <h1>Desa Talagasari</h1>
+                <hr>
+                <p>LOGIN</p>
+                <label for="">username</label>
+                <input type="text"  name="username" placeholder="username">
+                <label for="">Password</label>
+                <input type="password" name="password" placeholder="Password">
+                <button>Login</button>
+                <p>
+                    <a href="#">Forgot Password?</a>
+                </p>
+            </form>
+        </div>
+        <div class="right">
+            <img src="Desa.jpg" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
